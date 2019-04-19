477ba21ceb32af263892342ac74a6df0adb5ea47
rjsmall90 <rjsmall90@gmail.com>
2019-04-19 12:43:31 -0400

    json scripts added-ready to deploy

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -14,8 +14,9 @@ export class Portfolio extends Component {
                   <h5>Portfolio</h5>
                   <h1>Check Out Some of My Work!</h1>
                   <p className="lead">A collection of some of my recent projects, web applications, and designs</p>
-                <div>
-                  <Card class="cards">
+                <div className="row services-content">
+                  <div className="cards-alignment">
+                  <Card id="service-content" className="cards">
                     <CardActionArea target ="_blank" href="https://www.github.com/rjsmall90/rjsresume">
                     <CardMedia class="cards-media">
                       <img src="images/Rsmall.jpg"></img>
@@ -31,19 +32,19 @@ export class Portfolio extends Component {
                     <CardMedia className="cards-media">
                       <img src="images/final_spitball.jpg"></img>
                     </CardMedia>
-                    <h2>Project Spitball - Teacher Rating Service/Social Media</h2>
-                    <CardContent>
-                    <p>In order to learn how to build a web applicaton from top to bottom: using Java, JavaScript, Spring/SpringBoot, MySQL, & Ionic 3 built a web application using MVC design concepts. The application is designed to allow users to rate their teachers per school and district.</p> 
+                    <h2>Project Spitball</h2>
+                    <CardContent className="cards-content">
+                    <p>Teacher Rating Service/Social Media. In order to learn how to build a web applicaton from top to bottom: using Java, JavaScript, Spring/SpringBoot, MySQL, & Ionic 3 built a web application using MVC design concepts. The application is designed to allow users to rate their teachers per school and district.</p> 
                     </CardContent>
                     </CardActionArea>
                   </Card>
                   <Card className="cards">
                     <CardActionArea target="_blank" href="https://github.com/rjsmall90/JavaJawn">
                     <CardMedia className="cards-media">
-                      <img src="images/javalogo.jpg"></img>
+                      <img src="images/java_logo.jpg"></img>
                     </CardMedia>
                     <h2>JavaJawn</h2>
-                    <CardContent>
+                    <CardContent className="cards-content">
                     <p>using Java, Spring/SpringBoot, & PostgreSQL, built a functioning, full-stack scheduling application to better learn the Spring MVC design pattern and using React for the front-end.</p> 
                     </CardContent>
                     </CardActionArea>
@@ -51,11 +52,11 @@ export class Portfolio extends Component {
                   <Card className="cards">
                     <CardActionArea target="_blank" href="https://github.com/rjsmall90/FoodReyes">
                     <CardMedia className="card-media">
-                      <img src="images/"></img>
+                      <img src="images/menu.jpg" className="card-media"></img>
                     </CardMedia>
-                    <h2>FoodReyes-touch screen application for registering inventory and purchasing products</h2>
-                    <CardContent>
-                    <p>working with a partner-using Java, Spring/SpringBoot, & React , building a .</p> 
+                    <h2>FoodReyes</h2>
+                    <CardContent className="card-content">
+                    <p>-touch screen application for registering inventory and purchasing products. Through collaboration-using Java, Spring/SpringBoot, & React , building an inventory and purchasing mobile application</p> 
                     </CardContent>
                     </CardActionArea>
                   </Card>
@@ -71,10 +72,10 @@ export class Portfolio extends Component {
                     </CardActionArea>
                   </Card>
                 </div>
-              </div> 
-                
+               </div> 
+               </div>
+               </div>
               </div> {/* /section-intro*/} 
-            </div>
             </React.Fragment>
           );
         }
